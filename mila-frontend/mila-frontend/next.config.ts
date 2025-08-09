5661f626dbdbb0df32c42bc6a5755ff4d4966e44
ehti4485 <ehtisham.smm@gmail.com>
2025-08-09 14:36:46 +0500

    Disabled ESLint during build in next.config.ts

--- mila-frontend/mila-frontend/next.config.ts
+++ mila-frontend/mila-frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Build ke waqt ESLint errors ignore karega
+  },
 };
 
 export default nextConfig;
+
